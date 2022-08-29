7b6949318429f70b6cb33bee091c1bea708d6ab5
Rafael Guzman <rega1237@gmail.com>
2022-08-29 18:36:36 -0400

    add routes users, foods and recipes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users do
+    resources :foods
+    resources :recipes
+  end
 end
