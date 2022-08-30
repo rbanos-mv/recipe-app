66ee49e0b14ffb83a5b521fc2e7e9a8075886938
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-30 04:11:36 -0500

    Refactor routes - Create HomeController

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # implement
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'home#index'
+  get 'public_recipes', to: 'home#public_recipes'
+  get 'shopping_list', to: 'home#shopping_list'
 
-  resources :users do
-    resources :foods
-    resources :recipes
+  resources :foods, except: :update
+
+  resources :recipes, except: :update do
+    resources :recipe_foods, only: %i[create destroy update]
   end
 end
