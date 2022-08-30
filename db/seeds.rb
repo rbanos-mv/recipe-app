25aa4e5ed2e3ea8dbb8913b70da92a0b4fc4d0e1
Rafael Guzman <rega1237@gmail.com>
2022-08-30 09:11:51 -0400

    add more data to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
 user0 = User.create(name: 'Rafael')
+user1 = User.create(name: 'Roberto')
 
 food0 = Food.create(user: user0, name: 'Apple', measurement_unit: 'grams', price: 5, quantity: 50)
 food1 = Food.create(user: user0, name: 'Pineapple', measurement_unit: 'grams', price: 1, quantity: 20)
@@ -17,3 +19,9 @@ recipe0 = Recipe.create(user: user0, name: 'Recipe 2', preparation_time: '1 hour
 RecipeFood.create(quantity: 20, recipe: recipe0, food: food0)
 RecipeFood.create(quantity: 10, recipe: recipe0, food: food1)
 RecipeFood.create(quantity: 2, recipe: recipe0, food: food2)
+
+recipe1 = Recipe.create(name: 'Pizza', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user0)
+Recipe.create(name: 'Cesar Salad', preparation_time: 0.5, cooking_time: 0.2, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user1)
+Recipe.create(name: 'Tomato Soup', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user0)
+Recipe.create(name: 'Roast beef', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user1)
+
