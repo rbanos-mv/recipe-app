8a712d2ec93ef0937ad67f835dd247f17f91059f
Rafael Guzman <rega1237@gmail.com>
2022-08-30 15:06:14 -0400

    add more data to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,15 @@ food0 = Food.create(user: user0, name: 'Apple', measurement_unit: 'grams', price
 food1 = Food.create(user: user0, name: 'Pineapple', measurement_unit: 'grams', price: 1, quantity: 20)
 food2 = Food.create(user: user0, name: 'Chicken breast', measurement_unit: 'unit', price: 2, quantity: 15)
 
-recipe0 = Recipe.create(user: user0, name: 'Recipe 2', preparation_time: '1 hour', cooking_time: '1 hour',
+recipe0 = Recipe.create(user: user0, name: 'Recipe 2', preparation_time: 1, cooking_time: 1,
                         description: 'Steps go here', public: true)
 
 RecipeFood.create(quantity: 20, recipe: recipe0, food: food0)
 RecipeFood.create(quantity: 10, recipe: recipe0, food: food1)
 RecipeFood.create(quantity: 2, recipe: recipe0, food: food2)
 
-recipe1 = Recipe.create(name: 'Pizza', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user0)
-Recipe.create(name: 'Cesar Salad', preparation_time: 0.5, cooking_time: 0.2, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user1)
+recipe1 = Recipe.create(name: 'Pizza', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user0, public: false)
+Recipe.create(name: 'Cesar Salad', preparation_time: 0.5, cooking_time: 0.2, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user1, public: false)
 Recipe.create(name: 'Tomato Soup', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user0)
 Recipe.create(name: 'Roast beef', preparation_time: 1, cooking_time: 1.5, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor, neque non vehicula laoreet, leo nulla pellentesque odio, in malesuada lectus nisi sed elit. Sed id justo ut justo aliquam.", user: user1)
 
