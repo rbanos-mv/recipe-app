0667035597c2fb41681485dab5cb29a8b9fc8a44
Rafael Guzman <rega1237@gmail.com>
2022-08-30 15:06:30 -0400

    create and style recipe index and show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.includes([:user]).where(user: params[:user_id])
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = RecipeFood.includes([:food]).where(recipe_id: params[:id])
+  end
+
+  def destroy
+    @recipes = Recipe.find(params[:id])
+
+    @recipes.destroy
+    redirect_to recipes_path
+  end
+
+  def edit
+    if self.public
+      self.public = false
+    else
+      self.public = true
+    end
+  end
 end
