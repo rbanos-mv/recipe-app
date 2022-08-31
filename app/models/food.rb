71edc17082ba2b69b168548e53c46a0fe788fae2
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-31 13:41:18 -0500

    Add has_many recipe_foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods
 
   validates :name, presence: true
   validates :measurement_unit, presence: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods
+
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
