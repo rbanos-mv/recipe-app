8254be9f149441a30cb7fb7c51f8ccc05eeefdde
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-31 14:32:37 -0500

    Create HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
+  # before_action :authenticate_user!
+  # load_and_authorize_resource
+
+  include HomeHelper
+
   def index
     # implement
+  end
+
+  def read
+    p '==============================000'
+    authorize! :read, current_user
+  end
+
+  def shopping_list
+    @items, @total = calculate_shopping
   end
 end
